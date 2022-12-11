4c63e0a624553871981eaa8fa1d5e87fcf045431
endorpersand <daendrdragon@gmail.com>
2022-12-10 21:38:59 -0500

    improve readability in day 1 hs soln

--- hs/day-01.hs
+++ hs/day-01.hs
@@ -1,10 +1,10 @@
 import Data.List (sort)
 main = do
     text <- readFile "inputs/1.txt"
-    let partitions = sumSequences $ lines text
+    let partitions = (sumSequences . lines) text
     print $ maximum partitions
     let sortedPartitions = sort partitions
-    print $ sum $ take 3 $ reverse $ sort partitions
+    print $ (sum . take 3 . reverse . sort) partitions
 
 sumSequences :: [String] -> [Int]
 sumSequences [] = []
