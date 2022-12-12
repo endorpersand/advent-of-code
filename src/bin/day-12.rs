29015d69e0399d78550d21df7221c669cf62ad62
endorpersand <daendrdragon@gmail.com>
2022-12-12 02:09:36 -0500

    day 12 rs simplification

--- src/bin/day-12.rs
+++ src/bin/day-12.rs
@@ -26,47 +26,32 @@ fn main() {
     let end_x = field[end_y].iter().position(|&e| e == E).unwrap();
 
     let mut frontier = Frontier::new(field.clone(), (start_x, start_y), (end_x, end_y));
-    let path = frontier.find_path().expect("Could not find path");
-    // println!("{:?}", path);
-    println!("{:?}", path.len() - 1);
+    let path_len = frontier.find_path_len().expect("Could not find path");
+    println!("{:?}", path_len);
 
     let (width, height) = (field[0].len(), field.len());
     let min_path = (0..width)
         .flat_map(|x| (0..height).map(move |y| (x, y)))
         .filter(|&(x, y)| field[y][x] <= 1)
         .map(|start| Frontier::new(field.clone(), start, (end_x, end_y)))
-        .flat_map(|mut f| f.find_path())
-        .map(|p| p.len() - 1)
+        .flat_map(|mut f| f.find_path_len())
         .min();
     println!("{min_path:?}")
 }
 
 type Coord = (usize, usize);
 
-#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Clone, Copy)]
-struct NodeData {
-    prio: usize,
-    pos: Coord,
-    parent: Option<Coord>,
-    start_dist: usize
-}
-
 struct Frontier {
     field: Vec<Vec<usize>>,
-    frontier: BinaryHeap<Reverse<NodeData>>,
-    traversed: HashMap<Coord, NodeData>,
+    frontier: BinaryHeap<Reverse<(usize, Coord)>>,
+    traversed: HashMap<Coord, usize>,
     target: Coord
 }
 
 impl Frontier {
     fn new(field: Vec<Vec<usize>>, start: Coord, end: Coord) -> Self {
         let mut frontier = BinaryHeap::new();
-        frontier.push(Reverse(NodeData {
-            prio: 0,
-            pos: start,
-            parent: None,
-            start_dist: 0
-        }));
+        frontier.push(Reverse((0, start)));
 
         Self {
             field,
@@ -93,44 +78,30 @@ impl Frontier {
             (chk!(x), chk!(y + 1)),
             (chk!(x - 1), chk!(y)),
             (chk!(x + 1), chk!(y)),
-        ].into_iter()
-        .filter_map(|(a, b)| a.zip(b))
-        .filter(|(a, b)| a < &self.field[0].len() && b < &self.field.len())
-        .filter(move |&(a, b)| self.get(a, b) <= height + 1)
-        .collect()
+        ]
+            .into_iter()
+            .filter_map(|(a, b)| a.zip(b))
+            .filter(|(a, b)| a < &self.field[0].len() && b < &self.field.len())
+            .filter(move |&(a, b)| self.get(a, b) <= height + 1)
+            .collect()
     }
 
-    fn find_path(&mut self) -> Option<Vec<Coord>> {
-        while let Some(Reverse(nd @ NodeData { prio: _, pos, parent: _, start_dist })) = self.frontier.pop() {
-            self.traversed.insert(pos, nd);
+    fn find_path_len(&mut self) -> Option<usize> {
+        while let Some(Reverse((start_dist, pos))) = self.frontier.pop() {
+            self.traversed.insert(pos, start_dist);
             if pos == self.target {
-                let mut nodes = vec![];
-                
-                let mut n = pos;
-                nodes.push(n);
-                while let Some(parent) = self.traversed.get(&n).map(|nd| nd.parent).flatten() {
-                    nodes.push(parent);
-                    n = parent;
-                }
-
-                return Some(nodes.into_iter().rev().collect());
+                return Some(start_dist);
             }
 
             let nbs = self.neighbors(pos);
             for nb in nbs {
-                if let Some(NodeData { start_dist: tvsd, .. }) = self.traversed.get(&nb) {
-                    if *tvsd <= start_dist + 1 {
+                if let Some(traversed_sd) = self.traversed.get(&nb) {
+                    if *traversed_sd <= start_dist + 1 {
                         continue;
                     }
                 }
-                let nd = NodeData {
-                    prio: start_dist + 1,
-                    pos: nb,
-                    parent: Some(pos),
-                    start_dist: start_dist + 1
-                };
-                self.traversed.insert(nb, nd);
-                self.frontier.push(Reverse(nd));
+                self.traversed.insert(nb, start_dist + 1);
+                self.frontier.push(Reverse((start_dist + 1, nb)));
             }
         }
         
