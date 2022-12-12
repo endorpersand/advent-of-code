a759f1ebe18726abad92406c54982e302bd88424
endorpersand <daendrdragon@gmail.com>
2022-12-12 15:05:21 -0500

    day 12, rust, v3, be less of a clown

--- src/bin/day-12.rs
+++ src/bin/day-12.rs
@@ -18,6 +18,7 @@ fn main() {
                 .collect()
         })
         .collect();
+
     // println!("{field:?}");
     let start_y = field.iter().position(|row| row.contains(&S)).unwrap();
     let start_x = field[start_y].iter().position(|&e| e == S).unwrap();
@@ -25,16 +26,16 @@ fn main() {
     let end_y = field.iter().position(|row| row.contains(&E)).unwrap();
     let end_x = field[end_y].iter().position(|&e| e == E).unwrap();
 
-    let mut frontier = Frontier::new(field.clone(), (start_x, start_y), (end_x, end_y));
-    let path_len = frontier.find_path_len().expect("Could not find path");
-    println!("{:?}", path_len);
+    let mut frontier = Frontier::new(field.clone(), (end_x, end_y));
+    frontier.consume_frontier();
+
+    println!("{:?}", frontier.traversed.get(&(start_x, start_y)));
 
     let (width, height) = (field[0].len(), field.len());
     let min_path = (0..width)
         .flat_map(|x| (0..height).map(move |y| (x, y)))
         .filter(|&(x, y)| field[y][x] <= 1)
-        .map(|start| Frontier::new(field.clone(), start, (end_x, end_y)))
-        .flat_map(|mut f| f.find_path_len())
+        .flat_map(|pos| frontier.traversed.get(&pos).copied())
         .min();
     println!("{min_path:?}")
 }
@@ -44,20 +45,18 @@ type Coord = (usize, usize);
 struct Frontier {
     field: Vec<Vec<usize>>,
     frontier: BinaryHeap<Reverse<(usize, Coord)>>,
-    traversed: HashMap<Coord, usize>,
-    target: Coord
+    traversed: HashMap<Coord, usize>
 }
 
 impl Frontier {
-    fn new(field: Vec<Vec<usize>>, start: Coord, end: Coord) -> Self {
+    fn new(field: Vec<Vec<usize>>, start: Coord) -> Self {
         let mut frontier = BinaryHeap::new();
         frontier.push(Reverse((0, start)));
 
         Self {
             field,
             frontier,
-            traversed: HashMap::new(),
-            target: end
+            traversed: HashMap::new()
         }
     }
 
@@ -67,31 +66,24 @@ impl Frontier {
 
     fn neighbors(&self, (x, y): Coord) -> Vec<Coord> {
         let height = self.get(x, y);
-        macro_rules! chk {
-            ($a:ident + $b:expr) => { $a.checked_add($b) };
-            ($a:ident - $b:expr) => { $a.checked_sub($b) };
-            ($a:ident) => { Some($a) };
-        }
-
+        let x = x as isize;
+        let y = y as isize;
         [
-            (chk!(x), chk!(y - 1)),
-            (chk!(x), chk!(y + 1)),
-            (chk!(x - 1), chk!(y)),
-            (chk!(x + 1), chk!(y)),
+            (x, y - 1),
+            (x, y + 1),
+            (x - 1, y),
+            (x + 1, y),
         ]
             .into_iter()
-            .filter_map(|(a, b)| a.zip(b))
-            .filter(|(a, b)| a < &self.field[0].len() && b < &self.field.len())
-            .filter(move |&(a, b)| self.get(a, b) <= height + 1)
+            .filter_map(|(a, b)| usize::try_from(a).ok().zip(usize::try_from(b).ok()))
+            .filter(|&(a, b)| a < self.field[0].len() && b < self.field.len())
+            .filter(move |&(a, b)| self.get(a, b) + 1 >= height)
             .collect()
     }
 
-    fn find_path_len(&mut self) -> Option<usize> {
+    fn consume_frontier(&mut self) {
         while let Some(Reverse((start_dist, pos))) = self.frontier.pop() {
             self.traversed.insert(pos, start_dist);
-            if pos == self.target {
-                return Some(start_dist);
-            }
 
             let nbs = self.neighbors(pos);
             for nb in nbs {
@@ -104,7 +96,5 @@ impl Frontier {
                 self.frontier.push(Reverse((start_dist + 1, nb)));
             }
         }
-        
-        None
     }
 }
