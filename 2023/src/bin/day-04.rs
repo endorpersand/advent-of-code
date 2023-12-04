cc170c82d968711d1bd689bea72ae7e11483f824
endorpersand <daendrdragon@gmail.com>
2023-12-04 12:26:04 -0500

    day 4 rs; eliminate hash map

--- 2023/src/bin/day-04.rs
+++ 2023/src/bin/day-04.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, HashMap};
+use std::collections::HashSet;
 
 use once_cell::sync::Lazy;
 use regex::Regex;
@@ -6,11 +6,11 @@ use regex::Regex;
 fn main() {
     let txt = std::fs::read_to_string("inputs/04.txt").unwrap();
     
-    let n_wins: HashMap<_, _> = txt.lines()
+    let n_wins: Vec<_> = txt.lines()
         .map(parse_card_basic)
         .collect();
 
-    let out: usize = n_wins.values()
+    let out: usize = n_wins.iter()
         .map(|&hits| if hits != 0 { 1 << (hits - 1) } else { 0 })
         .sum();
     println!("{out}");
@@ -18,26 +18,19 @@ fn main() {
     let n_lines = txt.lines().count();
     let mut ctr = vec![1usize; n_lines];
     for i in 0..n_lines {
-        let n_cards = ctr[i];
-        let n_matches = n_wins[&(i + 1)];
-
         #[allow(clippy::needless_range_loop)]
-        for j in (i + 1)..(i + 1 + n_matches) {
-            ctr[j] += n_cards;
+        for j in (i + 1)..(i + 1 + n_wins[i]) {
+            ctr[j] += ctr[i];
         }
     }
     println!("{}", ctr.iter().sum::<usize>());
 }
 
-fn parse_card_basic(line: &str) -> (usize, usize) {
+fn parse_card_basic(line: &str) -> usize {
     static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\d+").unwrap());
 
-    let (number, card) = line.split_once(": ").unwrap();
-    let cno = RE.find(number).unwrap()
-        .as_str()
-        .parse()
-        .unwrap();
-
+    let (_, card) = line.split_once(": ").unwrap();
+    
     let (winning, owned) = card.split_once(" | ").unwrap();
 
     let winning_numbers: HashSet<usize> = RE.find_iter(winning)
@@ -47,8 +40,7 @@ fn parse_card_basic(line: &str) -> (usize, usize) {
         .map(|t| t.as_str().parse().unwrap())
         .collect();
 
-    let matches = winning_numbers.intersection(&owned_numbers)
-        .count();
-
-    (cno, matches)
+    winning_numbers
+        .intersection(&owned_numbers)
+        .count()
 }
