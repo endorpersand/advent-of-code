a7003ed545eb8b0de6ef0a1e7376c7ee57bb0bab
endorpersand <daendrdragon@gmail.com>
2023-12-06 00:25:22 -0500

    day 6 rs

---
+++ 2023/src/bin/day-06.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let txt = std::fs::read_to_string("inputs/06.txt").unwrap();
+    let State { time, dist } = parse2(&txt); // j switch between parse and parse2
+
+    let out: usize = std::iter::zip(&time, &dist)
+        .map(|(&t, &d)| {
+            let t = t as f64;
+            let d = d as f64;
+
+            let half_t = t / 2.;
+            let discrim = (t.powi(2) - 4. * d).sqrt() / 2.;
+
+            let min = (half_t - discrim).max(0.);
+            let max = half_t + discrim;
+            
+            let min_next = min.ceil();
+            let min_int = if min_next == min { (min_next as usize) + 1 } else { min_next as usize };
+            let max_prev = max.floor();
+            let max_int = if max_prev == max { (max_prev as usize) - 1 } else { max_prev as usize };
+
+            min_int ..= max_int
+        })
+        .map(|c| dbg!(c.count()))
+        .product();
+    println!("{out}");
+}
+
+#[derive(Debug)]
+struct State {
+    time: Vec<usize>,
+    dist: Vec<usize>
+}
+fn parse(file: &str) -> State {
+    let (time_line, dist_line) = file.split_once('\n').unwrap();
+    
+    let mut time_it = time_line.split_whitespace();
+    time_it.next();
+    let time = time_it.map(|s| s.parse().unwrap()).collect();
+    
+    let mut dist_it = dist_line.split_whitespace();
+    dist_it.next();
+    let dist = dist_it.map(|s| s.parse().unwrap()).collect();
+
+    State { time, dist }
+}
+fn parse2(file: &str) -> State {
+    let (time_line, dist_line) = file.split_once('\n').unwrap();
+    
+    let mut time_it = time_line.split_whitespace();
+    time_it.next();
+    let time = time_it.collect::<String>().parse().unwrap();
+    
+    let mut dist_it = dist_line.split_whitespace();
+    dist_it.next();
+    let dist = dist_it.collect::<String>().parse().unwrap();
+
+    State { time: vec![time], dist: vec![dist] }
+}
