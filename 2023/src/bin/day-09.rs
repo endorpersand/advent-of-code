eb0c2cc37a43d7beba8e6697edb3f32402c60875
endorpersand <daendrdragon@gmail.com>
2023-12-09 00:16:36 -0500

    day 9 rs

---
+++ 2023/src/bin/day-09.rs
@@ -0,0 +1,60 @@
+fn main() {
+    let txt = std::fs::read_to_string("inputs/09.txt").unwrap();
+    let State { data } = parse(&txt);
+
+    let a: isize = data.iter()
+        .map(|line| compute_next(line))
+        .sum();
+    println!("{a:?}");
+    
+    let a: isize = data.iter()
+        .map(|line| compute_prev(line))
+        .sum();
+    println!("{a:?}");
+}
+
+#[derive(Debug)]
+struct State {
+    data: Vec<Vec<isize>>
+}
+fn parse(file: &str) -> State {
+    let data = file.lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|lit| lit.parse().unwrap())
+                .collect()
+        })
+        .collect();
+
+    State { data }
+}
+
+fn compute_next(data: &[isize]) -> isize {
+    let mut rhs = 0;
+
+    let mut remaining = data.to_vec();
+    while !remaining.is_empty() {
+        rhs += remaining.last().unwrap();
+        remaining = std::iter::zip(remaining.iter(), remaining.iter().skip(1))
+            .map(|(&a, &b)| b - a)
+            .collect();
+    }
+
+    rhs
+}
+fn compute_prev(data: &[isize]) -> isize {
+    let mut lhs = vec![];
+
+    let mut remaining = data.to_vec();
+    while !remaining.is_empty() {
+        lhs.push(*remaining.first().unwrap());
+        remaining = std::iter::zip(remaining.iter(), remaining.iter().skip(1))
+            .map(|(&a, &b)| b - a)
+            .collect();
+    }
+
+    lhs.into_iter()
+       .rev()
+       .reduce(|acc, cv| -acc + cv)
+       .unwrap()
+}
