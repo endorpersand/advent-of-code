42462bd20b297706700af9f8e35284be6a745fd4
endorpersand <daendrdragon@gmail.com>
2023-12-15 00:41:34 -0500

    day 15 rs, yknow, makin it slightly cleaner

--- 2023/src/bin/day-15.rs
+++ 2023/src/bin/day-15.rs
@@ -1,34 +1,32 @@
 use indexmap::IndexMap;
 
 fn main() {
+    // PART A
     let txt = std::fs::read_to_string("inputs/15.txt").unwrap();
-    let out: usize = txt.split(',')
-        .map(|s| hash(s) as usize)
-        .sum();
+    let out: usize = txt.split(',').map(hash).sum();
     println!("{out}");
 
+    // PART B
     let mut cells: [_; 256] = std::array::from_fn(|_| IndexMap::new());
     for instr in txt.split(',') {
-        if let Some((label, len)) = instr.split_once('=') {
-            cells[hash(label) as usize].insert(label, len.parse::<u8>().unwrap());
-        } else {
-            let label = instr.strip_suffix('-').unwrap();
-            cells[hash(label) as usize].shift_remove(&label);
-        }
+        let (label, len) = instr.split_once(|c: char| c.is_ascii_punctuation()).unwrap();
+        match len.parse::<usize>() {
+            Ok(t)  => cells[hash(label)].insert(label, t),
+            Err(_) => cells[hash(label)].shift_remove(&label),
+        };
     }
     let out: usize = cells.into_iter()
-        .zip(1usize..)
-        .flat_map(|(cell, i)| {
+        .enumerate()
+        .flat_map(|(i, cell)| {
             cell.into_values()
-                .zip(1usize..)
-                .map(move |(len, j)| i * j * (len as usize))
+                .enumerate()
+                .map(move |(j, len)| (i + 1) * (j + 1) * len)
         })
         .sum();
     println!("{out}");
 }
 
 
-fn hash(s: &str) -> u8 {
-    s.bytes()
-        .fold(0, |acc, cv| acc.wrapping_add(cv).wrapping_mul(17))
+fn hash(s: &str) -> usize {
+    s.bytes().fold(0u8, |acc, cv| acc.wrapping_add(cv).wrapping_mul(17)) as usize
 }
