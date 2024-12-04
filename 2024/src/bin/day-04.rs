80bbbf75810bdad20348a27600ead7c8d206c4de
Henry Bui <henry.h.bui05@gmail.com>
2024-12-04 01:05:16 -0500

    d4 opt 1

--- 2024/src/bin/day-04.rs
+++ 2024/src/bin/day-04.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 fn main() {
     let input = std::fs::read_to_string("inputs/04.txt").unwrap();
-    soln1(&input);
+    soln2(&input);
 }
 
 #[allow(dead_code)]
@@ -61,6 +61,66 @@ fn soln1(input: &str) {
                         .is_some_and(|nc| set.contains(&nc))
                 })
         }).count())
+        .sum();
+    println!("{p2}");
+}
+
+#[allow(dead_code)]
+fn soln2(input: &str) {
+    let [mut x, mut m, mut a, mut s] = std::array::from_fn(|_| HashSet::new());
+
+    for (i, l) in input.lines().enumerate() {
+        for (j, c) in l.bytes().enumerate() {
+            match c {
+                b'X' => x.insert((i, j)),
+                b'M' => m.insert((i, j)),
+                b'A' => a.insert((i, j)),
+                b'S' => s.insert((i, j)),
+                _ => unreachable!(),
+            };
+        }
+    }
+
+    fn count_deltas<const N: usize>(
+        (i, j): (usize, usize), 
+        sets: [&HashSet<(usize, usize)>; N], 
+        deltas: &[[(isize, isize); N]]
+    ) -> usize {
+        deltas.iter()
+            .filter(|&&group| {
+                std::iter::zip(sets, group).all(|(set, (di, dj))| {
+                    i.checked_add_signed(di).zip(j.checked_add_signed(dj)) // coord in bounds
+                        .is_some_and(|nc| set.contains(&nc)) // is in set
+                })
+            })
+            .count()
+    }
+
+    const DELTAS: &[[(isize, isize); 3]] = &[
+        [(0, 1), (0, 2), (0, 3)],
+        [(1, 0), (2, 0), (3, 0)],
+        [(1, 1), (2, 2), (3, 3)],
+        [(1, -1), (2, -2), (3, -3)],
+        [(0, -1), (0, -2), (0, -3)],
+        [(-1, 0), (-2, 0), (-3, 0)],
+        [(-1, -1), (-2, -2), (-3, -3)],
+        [(-1, 1), (-2, 2), (-3, 3)]
+    ];
+    
+    let p1: usize = x.iter()
+        .map(|&c| count_deltas(c, [&m, &a, &s], DELTAS))
+        .sum();
+    println!("{p1}");
+
+    const DELTAS2: &[[(isize, isize); 4]] = &[
+        [(-1, -1), (-1, 1), (1, -1), (1, 1)],
+        [(-1, -1), (1, -1), (-1, 1), (1, 1)],
+        [(1, -1), (1, 1), (-1, -1), (-1, 1)],
+        [(-1, 1), (1, 1), (-1, -1), (1, -1)],
+    ];
+
+    let p2: usize = a.iter()
+        .map(|&c| count_deltas(c, [&m, &m, &s, &s], DELTAS2))
         .sum();
     println!("{p2}");
 }
