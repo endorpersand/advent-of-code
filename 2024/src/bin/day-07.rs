6e12f4f13ab09b3b60146642a5c1221d7a5ca88c
Henry Bui <henry.h.bui05@gmail.com>
2024-12-07 00:22:08 -0500

    d7 switch to usize

--- 2024/src/bin/day-07.rs
+++ 2024/src/bin/day-07.rs
@@ -4,8 +4,8 @@ fn main() {
 }
 
 struct Data {
-    inputs: Vec<isize>,
-    output: isize
+    inputs: Vec<usize>,
+    output: usize
 }
 #[allow(dead_code)]
 fn soln1(input: &str) {
@@ -14,7 +14,7 @@ fn soln1(input: &str) {
         for &input in data.inputs.iter().rev() {
             for output in std::mem::take(&mut possible_paths) {
                 if output % input == 0 { possible_paths.push(output / input); }
-                possible_paths.push(output - input);
+                if let Some(diff) = output.checked_sub(input) { possible_paths.push(diff); }
             }
         }
         
@@ -24,10 +24,11 @@ fn soln1(input: &str) {
         let mut possible_paths = vec![data.output];
         for &input in data.inputs.iter().rev() {
             for output in std::mem::take(&mut possible_paths) {
-                let digits = input.checked_ilog10().unwrap_or(0) + 1;
-                if output % 10isize.pow(digits) == input { possible_paths.push(output / 10isize.pow(digits)); }
                 if output % input == 0 { possible_paths.push(output / input); }
-                possible_paths.push(output - input);
+                if let Some(diff) = output.checked_sub(input) { possible_paths.push(diff); }
+
+                let digits = input.checked_ilog10().unwrap_or(0) + 1;
+                if output % 10usize.pow(digits) == input { possible_paths.push(output / 10usize.pow(digits)); }
             }
         }
         
@@ -45,12 +46,12 @@ fn soln1(input: &str) {
 
     let (possible, not_possible): (Vec<_>, _) = data.iter().partition(|d| data_possible(d));
 
-    let p1: isize = possible.into_iter()
+    let p1: usize = possible.into_iter()
         .map(|d| d.output)
         .sum();
     println!("{p1}");
 
-    let p2p: isize = not_possible.into_iter()
+    let p2p: usize = not_possible.into_iter()
         .filter(|d| data_possible2(d))
         .map(|d| d.output)
         .sum();
