cb32df0c52258d840f9afd63936576b67509c658
Henry Bui <henry.h.bui05@gmail.com>
2024-12-08 00:41:22 -0500

    d8 use less lines you buffoon

--- 2024/src/bin/day-08.rs
+++ 2024/src/bin/day-08.rs
@@ -1,5 +1,4 @@
 use std::collections::{HashMap, HashSet};
-use std::num::NonZeroU8;
 
 fn main() {
     let input = std::fs::read_to_string("inputs/08.txt").unwrap();
@@ -12,7 +11,7 @@ fn compute_delta((lr, lc): Position, (rr, rc): Position) -> PosDelta {
     (rr.wrapping_sub(lr) as isize, rc.wrapping_sub(lc) as isize)
 }
 fn ray_sequence(start: Position, (dr, dc): PosDelta) -> impl Iterator<Item = Position> {
-    std::iter::successors(Some(start), move |&(sr, sc)| Some((sr.wrapping_add_signed(dr), sc.wrapping_add_signed(dc))))
+    std::iter::successors(Some(start), move |&(r, c)| Some((r.wrapping_add_signed(dr), c.wrapping_add_signed(dc))))
 }
 fn in_grid((r, c): Position, n_rows: usize, n_cols: usize) -> bool {
     (0..n_rows).contains(&r) && (0..n_cols).contains(&c)
@@ -20,20 +19,15 @@ fn in_grid((r, c): Position, n_rows: usize, n_cols: usize) -> bool {
 #[allow(dead_code)]
 fn soln1(input: &str) {
     let grid: Vec<Vec<_>> = input.lines()
-        .map(|s| {
-            s.bytes()
-                .map(|e| if e != b'.' { e } else { 0 })
-                .map(NonZeroU8::new)
-                .collect()
-        })
+        .map(|s| s.as_bytes().to_vec())
         .collect();
     let n_rows = grid.len();
     let n_cols = grid[0].len();
 
     let mut groups: HashMap<_, _> = HashMap::new();
     for (r, row) in grid.iter().enumerate() {
-        for (c, mel) in row.iter().enumerate() {
-            if let &Some(el) = mel {
+        for (c, &el) in row.iter().enumerate() {
+            if el != b'.' {
                 groups.entry(el).or_insert_with(Vec::new).push((r, c));
             }
         }
