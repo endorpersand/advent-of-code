442eb432287a2f712c867ba1a2667d797cfc7260
Henry Bui <henry.h.bui05@gmail.com>
2024-12-09 00:36:39 -0500

    refactor benchmarking

--- 2024/benches/benchmarks.rs
+++ 2024/benches/benchmarks.rs
@@ -1,11 +1,11 @@
+use aoc_2024::optimized::day_06;
 use criterion::{criterion_group, criterion_main, Criterion};
-use aoc_2024::{d6p1, d6p2};
 
-pub fn day06(c: &mut Criterion) {
+pub fn day_06(c: &mut Criterion) {
     let input = std::fs::read_to_string("inputs/06.txt").unwrap();
-    c.bench_function("test p1", |b| b.iter(|| d6p1(&input)));
-    c.bench_function("test p2", |b| b.iter(|| d6p2(&input)));
+    c.bench_function("test d6p1", |b| b.iter(|| day_06::part1(&input)));
+    c.bench_function("test d6p2", |b| b.iter(|| day_06::part2(&input)));
 }
 
-criterion_group!(benches, day06);
+criterion_group!(benches, day_06);
 criterion_main!(benches);
