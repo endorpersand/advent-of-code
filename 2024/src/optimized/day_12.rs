72bc59d514061fd48b487af66e46474bdf4bccee
Henry Bui <henry.h.bui05@gmail.com>
2024-12-12 02:55:51 -0500

    d12 hella code simplification

--- 2024/src/optimized/day_12.rs
+++ 2024/src/optimized/day_12.rs
@@ -4,23 +4,14 @@ fn offset<T>(grid: &Grid<T>, (r, c): Position, (dr, dc): PosDelta) -> Option<Pos
     let np @ (nr, nc) = (r.wrapping_add_signed(dr), c.wrapping_add_signed(dc));
     ((0..grid.len()).contains(&nr) && (0..grid[0].len()).contains(&nc)).then_some(np)
 }
-fn find_neighbors<T>(grid: &Grid<T>, (r, c): Position) -> impl Iterator<Item=Position> + '_ {
-    const DIRS: [PosDelta; 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
-    DIRS.into_iter()
-        .filter_map(move |delta| offset(grid, (r, c), delta))
-}
 
 const N: usize = 140;
 struct Grid<T> {
     inner: Vec<[T; N]>
 }
 impl<T: Copy> Grid<T> {
-    fn new(rows: usize) -> Self where T: Default {
-        let inner = std::iter::repeat_with(|| std::array::from_fn(|_| Default::default()))
-            .take(rows)
-            .collect();
-
-        Self { inner }
+    fn new(default: T, rows: usize) -> Self {
+        Self { inner: vec![[default; N]; rows] }
     }
 }
 impl Grid<bool> {
@@ -48,24 +39,6 @@ impl<T> std::ops::DerefMut for Grid<T> {
     }
 }
 
-#[derive(Default, Debug)]
-struct Group {
-    points: usize,
-    // part 1
-    inner_edges: usize,
-    // part 2
-    corners: usize
-}
-impl Group {
-    fn area(&self) -> usize {
-        self.points
-    }
-    fn perimeter(&self) -> usize {
-        self.points * 4 - self.inner_edges
-    }
-}
-
-// TODO: Make this contiguous
 fn parse(input: &str) -> Grid<u8> {
     let inner = input.lines()
         .map(|l| l.bytes().collect::<Vec<_>>())
@@ -77,7 +50,7 @@ fn parse(input: &str) -> Grid<u8> {
 }
 pub fn part1(input: &str) -> usize {
     let grid = parse(input);
-    let mut explored = Grid::new(grid.len());
+    let mut explored = Grid::new(false, grid.len());
     let mut frontier = vec![];
     let mut accum = 0;
 
@@ -86,14 +59,15 @@ pub fn part1(input: &str) -> usize {
         frontier.clear();
         frontier.push(start);
         
-        let mut group = Group::default();
+        let mut area = 0;
+        let mut perimeter = 0;
         while let Some(p @ (r, c)) = frontier.pop() {
             let value = grid[r][c];
             let mut inners = 0;
 
-            // For each neighbor (which is in the group),
-            // track an inner-edge and add to frontier
-            find_neighbors(&grid, p)
+            [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                .into_iter()
+                .filter_map(|delta| offset(&grid, p, delta))
                 .filter(|&(nr, nc)| grid[nr][nc] == value)
                 .for_each(|np| {
                     inners += 1;
@@ -102,11 +76,11 @@ pub fn part1(input: &str) -> usize {
                     }
                 });
 
-            group.points += 1;
-            group.inner_edges += inners;
+            area += 1;
+            perimeter += 4 - inners;
         }
 
-        accum += group.area() * group.perimeter();
+        accum += area * perimeter;
     }
 
     accum
@@ -114,7 +88,7 @@ pub fn part1(input: &str) -> usize {
 
 pub fn part2(input: &str) -> usize {
     let grid = parse(input);
-    let mut explored = Grid::new(grid.len());
+    let mut explored = Grid::new(false, grid.len());
     let mut frontier = vec![];
     let mut accum = 0;
 
@@ -123,10 +97,11 @@ pub fn part2(input: &str) -> usize {
         frontier.clear();
         frontier.push(start);
         
-        let mut group = Group::default();
+        let mut area = 0;
+        let mut corners = 0;
         while let Some(p @ (r, c)) = frontier.pop() {
             let value = grid[r][c];
-            let neighbors = [
+            let [ul, uc, ur, cl, cr, bl, bc, br] = [
                 (-1, -1), // UL
                 (-1,  0), // UC
                 (-1,  1), // UR
@@ -136,13 +111,12 @@ pub fn part2(input: &str) -> usize {
                 ( 1,  0), // BC
                 ( 1,  1), // BR
             ].map(|delta| offset(&grid, p, delta).filter(|&(nr, nc)| grid[nr][nc] == value));
-            let [ul, uc, ur, cl, cr, bl, bc, br] = neighbors;
 
-            group.points += 1;
-            if uc.is_some() == cl.is_some() && (uc.is_none() || ul.is_none()) { group.corners += 1 };
-            if uc.is_some() == cr.is_some() && (uc.is_none() || ur.is_none()) { group.corners += 1 };
-            if bc.is_some() == cl.is_some() && (bc.is_none() || bl.is_none()) { group.corners += 1 };
-            if bc.is_some() == cr.is_some() && (bc.is_none() || br.is_none()) { group.corners += 1 };
+            area += 1;
+            corners += usize::from(uc.is_some() == cl.is_some() && (uc.is_none() || ul.is_none()));
+            corners += usize::from(uc.is_some() == cr.is_some() && (uc.is_none() || ur.is_none()));
+            corners += usize::from(bc.is_some() == cl.is_some() && (bc.is_none() || bl.is_none()));
+            corners += usize::from(bc.is_some() == cr.is_some() && (bc.is_none() || br.is_none()));
 
             [uc, cl, cr, bc].into_iter().flatten().for_each(|np| {
                 if explored.insert(np) {
@@ -151,7 +125,7 @@ pub fn part2(input: &str) -> usize {
             });
         }
 
-        accum += group.area() * group.corners;
+        accum += area * corners;
     }
 
     accum
