9738c79af504453c0258c7812ee9c882576f7660
Henry Bui <henry.h.bui05@gmail.com>
2024-12-17 02:10:05 -0500

    d17 cleanup

--- 2024/src/bin/day-17.rs
+++ 2024/src/bin/day-17.rs
@@ -4,6 +4,7 @@ fn main() {
     part2(&input);
 }
 
+#[derive(Default)]
 struct Machine {
     registers: [usize; 3],
     instructions: Vec<u8>,
@@ -12,6 +13,9 @@ struct Machine {
 }
 impl std::fmt::Display for Machine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        const INSTRS: [&str; 8] = ["adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv"];
+        const COMBOS: [&str; 8] = ["0", "1", "2", "3", "A", "B", "C", "?"];
+
         writeln!(f, "Reg A: {}", self.registers[0])?;
         writeln!(f, "Reg B: {}", self.registers[1])?;
         writeln!(f, "Reg C: {}", self.registers[2])?;
@@ -20,18 +24,11 @@ impl std::fmt::Display for Machine {
         self.instructions.chunks_exact(2)
             .try_for_each(|chunk| {
                 let &[ins, op] = chunk else { unreachable!() };
-                f.write_str([
-                    "adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv"
-                ][usize::from(ins)])?;
-                f.write_str(" ")?;
-
-                if [1, 3].contains(&ins) {
-                    write!(f, "{}", chunk[1])?;
-                } else {
-                    f.write_str([
-                        "0", "1", "2", "3", "A", "B", "C", "?"
-                    ][usize::from(op)])?;
-                }
+                write!(f, "{} ", INSTRS[usize::from(ins)])?;
+                match [1, 3].contains(&ins) {
+                    true => write!(f, "{op}"),
+                    false => write!(f, "{}", COMBOS[usize::from(op)])
+                }?;
                 writeln!(f)
             })?;
         writeln!(f)?;
@@ -55,63 +52,43 @@ impl Machine {
     fn run_instr(&mut self, instr: u8, op: u8) {
         match instr {
             // The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.
-            0 => {
-                self.registers[0] >>= self.get_op(op);
-            },
+            0 => self.registers[0] >>= self.get_op(op),
+
             // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
-            1 => {
-                self.registers[1] ^= op as usize;
-            },
+            1 => self.registers[1] ^= usize::from(op),
 
             // The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
-            2 => {
-                self.registers[1] = self.get_op(op) % 8
-            },
+            2 => self.registers[1] = self.get_op(op) % 8,
 
             // The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
-            3 => {
-                let a = self.registers[0];
-                if a != 0 {
-                    self.pc = op as usize;
-                }
+            3 => if self.registers[0] != 0 {
+                self.pc = usize::from(op);
             },
 
             // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
-            4 => {
-                // _ = self.get_op(op);
-                self.registers[1] ^= self.registers[2];
-            },
+            4 => self.registers[1] ^= self.registers[2],
 
             // The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
-            5 => {
-                self.output.push((self.get_op(op) % 8) as u8);
-            },
+            5 => self.output.push((self.get_op(op) % 8) as u8),
 
             // The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
-            6 => {
-                let n = self.registers[0];
-                self.registers[1] = n >> self.get_op(op);
-            },
+            6 => self.registers[1] = self.registers[0] >> self.get_op(op),
 
             // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
-            7 => {
-                let n = self.registers[0];
-                self.registers[2] = n >> self.get_op(op);
-            },
+            7 => self.registers[2] = self.registers[0] >> self.get_op(op),
 
             _ => unreachable!()
         };
     }
 
     fn next(&mut self) {
-        let [instr, op] = self.instructions[self.pc ..= self.pc+1] else { panic!() };
+        let [instr, op] = self.instructions[self.pc ..= self.pc+1] else { unreachable!() };
         self.pc += 2;
         self.run_instr(instr, op);
     }
     fn reset(&mut self) {
-        self.registers.fill(0);
-        self.pc = 0;
-        self.output.clear();
+        let m = std::mem::take(self);
+        self.instructions = m.instructions;
     }
     fn run(&mut self) {
         while self.pc < self.instructions.len() {
@@ -137,8 +114,7 @@ fn parse(input: &str) -> Machine {
     Machine {
         registers: registers.try_into().unwrap(),
         instructions,
-        pc: 0,
-        output: vec![]
+        ..Default::default()
     }
 }
 fn fold(vals: &[u8]) -> usize {
@@ -163,9 +139,8 @@ fn part2(input: &str) {
     // B ^= C;
     // A = A >> 3;
     // B ^= 7;
-    // out B;
-    // jmp;
-
+    // out B % 8;
+    // jnz;
 
     // B = A % 8;
     // B ^= 2;
@@ -173,58 +148,46 @@ fn part2(input: &str) {
     // A = A >> 3;
     // B ^= 7;
     // out B % 8;
-    // jmp;
+    // jnz;
     
     // ^ Note that this only works on one input, and no others.
 
-    let base = machine.instructions.clone();
     let mut accum = vec![];
     let mut frontier = vec![];
-    let mut i = 0;
-    'outer: loop {
-        for &n in base[..base.len() - i].iter().rev() {
-            let folded = fold(&accum);
-            let mut options = (0..8)
-                .filter(|i| (i ^ 5 ^ ((folded << 3 | i) >> (i ^ 2))) % 8 == usize::from(n));
+    let context_switch = |accum: &mut Vec<_>, frontier: &mut Vec<_>| {
+        // We hit a dead end, so return to one of the frontier options
+        let Some((oi, on)) = frontier.pop() else { panic!("no solution found") };
 
-            match options.next() {
-                Some(nn) => {
-                    // Push one of the options, and the rest to frontier (for later)
-                    accum.push(nn as u8);
-                    options.filter(|&o| o > nn)
-                        .for_each(|o| { frontier.push((i, o)); });
-                },
-                None => {
-                    // We hit a dead end, so return to one of the frontier options
-                    let Some((oi, on)) = frontier.pop() else { break 'outer };
+        accum.truncate(oi);
+        accum.push(on as u8);
+    };
+    loop {
+        if machine.instructions.len() == accum.len() {
+            machine.reset();
+            machine.registers[0] = fold(&accum);
+            machine.run();
 
-                    accum.truncate(oi);
-                    accum.push(on as u8);
-
-                    i = oi + 1;
-                    continue 'outer;
-                },
+            match machine.output == machine.instructions {
+                true => break,
+                false => context_switch(&mut accum, &mut frontier)
             }
-            i += 1;
+            continue;
         }
 
-        machine.reset();
-        machine.registers[0] = fold(&accum);
-        machine.run();
-        if machine.output != machine.instructions {
-            let Some((oi, on)) = frontier.pop() else { break 'outer };
-            accum.truncate(oi);
-            accum.push(on as u8);
-            i = oi;
-            continue 'outer;
+        let n = machine.instructions[machine.instructions.len() - 1 - accum.len()];
+        let folded = fold(&accum);
+        let mut options = (0..8)
+            .filter(|i| (i ^ 5 ^ ((folded << 3 | i) >> (i ^ 2))) % 8 == usize::from(n));
+
+        match options.next() {
+            Some(nn) => {
+                // Push one of the options, and the rest to frontier (for later)
+                accum.push(nn as u8);
+                frontier.extend(options.map(|o| (accum.len() - 1, o)));
+            },
+            None => context_switch(&mut accum, &mut frontier),
         }
-        break;
     }
-
-    machine.reset();
-    machine.registers[0] = fold(&accum);
-    machine.run();
-    assert_eq!(machine.output, machine.instructions);
+    
     println!("{}", fold(&accum));
-    // println!("{base:?}");
 }
