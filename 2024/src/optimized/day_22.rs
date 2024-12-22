263b9c29e4e0aef34e1401d2419c7989937d6063
Henry Bui <henry.h.bui05@gmail.com>
2024-12-22 01:43:42 -0500

    lower secrets to u32, simplify next_secret, factor out parse(...)

--- 2024/src/optimized/day_22.rs
+++ 2024/src/optimized/day_22.rs
@@ -1,19 +1,19 @@
 use rustc_hash::FxHashMap;
 
 #[allow(clippy::let_and_return)]
-fn next_secret(secret: usize) -> usize {
-    let secret = (secret ^ (secret <<  6)) & 0xFFFFFF;
-    let secret = (secret ^ (secret >>  5)) & 0xFFFFFF;
-    let secret = (secret ^ (secret << 11)) & 0xFFFFFF;
+fn next_secret(mut secret: u32) -> u32 {
+    secret ^= (secret <<  6) & 0xFFFFFF;
+    secret ^= (secret >>  5) & 0xFFFFFF;
+    secret ^= (secret << 11) & 0xFFFFFF;
     secret
 }
 
-fn secrets_iter(secret: usize) -> impl Iterator<Item = usize> {
+fn secrets_iter(secret: u32) -> impl Iterator<Item = u32> {
     std::iter::successors(Some(secret), |&n| Some(next_secret(n)))
 }
-fn all_quads(seq: &[usize]) -> impl DoubleEndedIterator<Item=(u32, u32)> + use<'_> {
+fn all_quads(seq: &[u32]) -> impl DoubleEndedIterator<Item=(u32, u32)> + use<'_> {
     seq.windows(5).map(|w| {
-        let value = (w[4] % 10) as u32;
+        let value = w[4] % 10;
         let group = std::array::from_fn(|j| {
             let s0 = (w[j + 1] % 10) as u8;
             let s1 = (w[j] % 10) as u8;
@@ -23,22 +23,20 @@ fn all_quads(seq: &[usize]) -> impl DoubleEndedIterator<Item=(u32, u32)> + use<'
     })
 }
 
-pub fn part1(input: &str) -> usize {
-    let secrets: Vec<usize> = input.lines()
+fn parse(input: &str) -> Vec<u32> {
+    input.lines()
         .flat_map(str::parse)
-        .collect();
-    
-    secrets.iter()
-        .map(|&n| secrets_iter(n).nth(2000).unwrap())
+        .collect()
+}
+pub fn part1(input: &str) -> u64 {
+    parse(input).into_iter()
+        .map(|n| secrets_iter(n).nth(2000).unwrap())
+        .map(u64::from)
         .sum()
 }
 pub fn part2(input: &str) -> u32 {
-    let secrets: Vec<usize> = input.lines()
-        .flat_map(str::parse)
-        .collect();
-
     let mut joined = FxHashMap::default();
-    for n in secrets {
+    for n in parse(input) {
         let seq: Vec<_> = secrets_iter(n).take(2001).collect();
         let map: FxHashMap<_, _> = all_quads(&seq).rev().collect();
         for (k, v) in map {
