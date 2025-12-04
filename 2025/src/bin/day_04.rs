b837cce091f795b2b7b81ac2e3c2d9a8de722754
Henry Bui <henry.h.bui05@gmail.com>
2025-12-04 00:33:07 -0500

    day 4 cleanup

--- 2025/src/bin/day_04.rs
+++ 2025/src/bin/day_04.rs
@@ -5,6 +5,18 @@ fn main() {
     soln(&input);
 }
 
+fn parse(input: &str) -> HashSet<Coord> {
+    input.lines()
+        .enumerate()
+        .flat_map(|(r, l)| {
+            l.bytes()
+                .enumerate()
+                .filter(|&(_, i)| i == b'@')
+                .map(move |(c, _)| (r, c))
+            })
+        .collect()
+}
+
 type Coord = (usize, usize);
 fn neighbors((r, c): Coord) -> [Coord; 8] {
     [
@@ -18,53 +30,33 @@ fn neighbors((r, c): Coord) -> [Coord; 8] {
         (r.wrapping_add_signed(1), c.wrapping_add_signed(1)),
     ]
 }
-fn parse(input: &str) -> HashSet<Coord> {
-    input.lines()
-        .enumerate()
-        .flat_map(|(r, l)| {
-            l.bytes()
-                .enumerate()
-                .filter(|&(_, i)| i == b'@')
-                .map(move |(c, _)| (r, c))
-            })
-        .collect()
+fn removables(grid: &HashSet<Coord>) -> impl Iterator<Item=Coord> {
+    grid.iter()
+        .filter(|&&c| {
+            let ncount = neighbors(c).into_iter().filter(|n| grid.contains(n)).count();
+            ncount < 4
+        })
+        .copied()
 }
+
 fn soln(input: &str) {
     let grid = parse(input);
 
     // part 1
-    let p1 = grid.iter()
-        .filter(|&&c| {
-            let ncount = neighbors(c).into_iter()
-                .filter(|n| grid.contains(n))
-                .count();
-
-            ncount < 4
-        })
-        .count();
+    let p1 = removables(&grid).count();
     println!("{p1}");
 
     // part 2
     let mut p2 = 0;
     let mut current = grid.clone();
-    loop {
-        let removed: HashSet<_> = current.iter()
-            .filter(|&&c| {
-                let ncount = neighbors(c).into_iter()
-                    .filter(|n| current.contains(n))
-                    .count();
-
-                ncount < 4
-            })
-                .copied()
-                .collect();
-        
-        if removed.is_empty() { break; }
+    while let removed = removables(&current).collect::<HashSet<_>>() 
+        && !removed.is_empty()
+    {
         p2 += removed.len();
-
         for r in removed {
             current.remove(&r);
         }
     }
+    
     println!("{p2}");
 }
