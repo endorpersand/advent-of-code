8ffbeec651109a663f4a52e2013e558299ad5cb1
Henry Bui <henry.h.bui05@gmail.com>
2025-12-08 00:54:04 -0500

    d08 revised

--- 2025/src/bin/day_08.rs
+++ 2025/src/bin/day_08.rs
@@ -1,7 +1,4 @@
-use std::cmp::Reverse;
-use std::collections::{BinaryHeap, HashMap};
-
-use disjoint::DisjointSetVec;
+use disjoint::DisjointSet;
 
 fn main() {
     let input = std::fs::read_to_string("inputs/08.txt").unwrap();
@@ -16,68 +13,56 @@ fn sq_dist(l: Vector, r: Vector) -> usize {
         .map(|(a, b)| a.abs_diff(b).pow(2))
         .sum()
 }
+fn dist_vec(vecs: &[Vector]) -> Vec<(usize, [usize; 2])> {
+    let mut distances: Vec<_> = vecs.iter().enumerate()
+        .flat_map(|(i, &v)| {
+            vecs[i + 1 ..].iter()
+                .zip(i + 1 ..)
+                .map(move |(&w, j)| (sq_dist(v, w), [i, j]))
+        })
+        .collect();
+
+    distances.sort();
+    distances
+}
+
 fn parse(input: &str) -> Vec<[usize; 3]> {
     input.lines()
         .map(|l| {
-            let b: Box<[_; 3]> = l.split(',')
-                .map(|s| s.parse().unwrap())
-                .collect::<Box<[_]>>()
-                .try_into()
-                .unwrap();
-
-            *b
+            let mut it = l.split(',');
+            std::array::from_fn(|_| it.next().unwrap().parse().unwrap())
         }).collect()
 }
 fn part1(input: &str) -> usize {
     const SHORTEST_N: usize = 1000;
     const TOP_N: usize = 3;
 
-    let mut vectors = parse(input);
-    let revmap = HashMap::<_, usize>::from_iter(vectors.iter().copied().zip(0..));
-    let mut distances = BinaryHeap::new();
-    let mut jboxes = DisjointSetVec::from(vectors.clone());
-
-    vectors.sort();
-    for (i, &a) in vectors.iter().enumerate() {
-        for &b in &vectors[i+1..] {
-            let [a, b] = if a < b { [a, b] } else { [b, a] };
-            distances.push((Reverse(sq_dist(a, b)), [a, b]));
-        }
+    let vectors = parse(input);
+    let distances = dist_vec(&vectors);
+    let mut jboxes = DisjointSet::with_len(vectors.len());
+    
+    for &(_, [i, j]) in &distances[0..SHORTEST_N] {
+        jboxes.join(i, j);
     }
 
-    for _ in 0..SHORTEST_N {
-        let Some((_, [a, b])) = distances.pop() else { break };
-        jboxes.join(revmap[&a], revmap[&b]);
-    }
-
-    let mut sets = jboxes.indices().sets();
+    let mut sets = jboxes.sets();
     sets.sort_by_key(|s| !s.len());
-    println!("{sets:?}");
     sets[0..TOP_N].iter().map(|s| s.len()).product()
 }
 fn part2(input: &str) -> usize {
-    let mut vectors = parse(input);
-    let revmap = HashMap::<_, usize>::from_iter(vectors.iter().copied().zip(0..));
-    let mut distances = BinaryHeap::new();
-    let mut jboxes = DisjointSetVec::from(vectors.clone());
+    let vectors = parse(input);
+    let distances = dist_vec(&vectors);
+    let mut jboxes = DisjointSet::with_len(vectors.len());
 
-    vectors.sort();
-    for (i, &a) in vectors.iter().enumerate() {
-        for &b in &vectors[i+1..] {
-            let [a, b] = if a < b { [a, b] } else { [b, a] };
-            distances.push((Reverse(sq_dist(a, b)), [a, b]));
-        }
-    }
-
-    fn count_sets<T>(v: &DisjointSetVec<T>) -> usize {
+    fn count_sets(v: &DisjointSet) -> usize {
         (0..v.len())
             .filter(|&i| v.root_of(i) == i)
             .count()
     }
 
-    while let Some((_, [a, b])) = distances.pop() {
-        if jboxes.join(revmap[&a], revmap[&b]) && count_sets(&jboxes) == 1 {
-            return a[0] * b[0];
+    for (_, [i, j]) in distances {
+        if jboxes.join(i, j) && count_sets(&jboxes) == 1 {
+            return vectors[i][0] * vectors[j][0];
         }
     }
     panic!("can't find");
